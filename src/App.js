a4b9ba42ab9a3ad15dba8fe5eecd13ce1f4f7cac
Caio Macbook <capoyares@gmail.com>
2019-12-27 17:45:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./assets/sass/index.scss";
+
+const App = () => {
+  return <h1 class="main-title">Webpack is so so so so fucking awesome</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "my-first-bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
